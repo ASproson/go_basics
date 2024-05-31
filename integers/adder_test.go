7c9388e55e6e82a2f017df0b3d4698d35d7f81b7
ASproson <a.sproson@outlook.com>
2024-05-31 11:34:20 +0100

    Integers with tests and Go docs

---
+++ integers/adder_test.go
@@ -0,0 +1,24 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+// ^ This comment ensure that the example is ran with go test -v
+// Examples automatically update our Go doc, ensuring it's always kept up to date
