4be30b672c731986ae5f28c5e56d11e45f63670e
ASproson <a.sproson@outlook.com>
2024-05-31 10:46:49 +0100

    Adds test comments

--- fundamentals/hello_test.go
+++ fundamentals/hello_test.go
@@ -12,3 +12,7 @@ func TestHello(t *testing.T) {
 }
 
 // go test runs the tests
+// test files must be named as s_test.go
+// test functions must start with the word Test
+// These functions take one argument only: t *testing.T, and to use testing we need to import it
+// t.Errorf prints out a message during the test
