764871b7b804e456a7923657ca7a34892641ca5f
ASproson <a.sproson@outlook.com>
2024-05-31 14:39:13 +0100

    Adds array sums

--- arraysSlices/sum_test.go
+++ arraysSlices/sum_test.go
@@ -1,6 +1,11 @@
 package arraysslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+// go test -cover gives how well our code is covered
 
 func TestSum(t *testing.T) {
 
@@ -19,4 +24,33 @@ func assertCorrectArraySum(t testing.TB, got int, want int, numbers []int) {
 	}
 }
 
-// go test -cover gives how well our code is covered
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("Make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("Safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+}
