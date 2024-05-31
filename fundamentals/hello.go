68e003649a8259ffaa355bd79bc66091749c7422
ASproson <a.sproson@outlook.com>
2024-05-31 10:55:33 +0100

    Resolves empty strings

--- fundamentals/hello.go
+++ fundamentals/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const hello = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		return "Hello, World"
+	}
+	return hello + name
 }
 
 func main() {
