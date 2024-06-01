3736c926295a6fefb8a6242ce36c123cf611e0eb
ASproson <a.sproson@outlook.com>
2024-06-01 18:49:40 +0100

    Adds triangles and refactors table test

--- structsMethodsInterfaces/Perimeter.go
+++ structsMethodsInterfaces/Perimeter.go
@@ -23,6 +23,15 @@ type Shape interface {
 	Area() float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
